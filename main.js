baa04d01a89586eee8c70d7b41021793f42aae4d
Daltonshults <dshults@cocc.edu>
2022-02-28 21:22:24 -0800

    Got manufacturers link and categories link to replace the contents of the main element with the contents of the arrays.

--- main.js
+++ main.js
@@ -9,23 +9,46 @@ const categories = [
 const manufacturers = [
   { id: 5, name: 'BMW' },
   { id: 2, name: 'Honda' },
-  { id: 1, name: 'Indian' } ,
+  { id: 1, name: 'Indian' },
   { id: 3, name: 'Triumph' },
   { id: 4, name: 'Victory' }
 ]
 
 function bindNavBarClickHandlers(event) {
   let categoriesLink = document.getElementById("lnk-categories")
+  let displayLink = document.getElementById("lnk-manufacturers")
+  displayLink.addEventListener("click", displayManufacturers)
   categoriesLink.addEventListener("click", displayCategories)
+  //displayLink.addEventListener("click", displayManufacturers)
 }
 
 function displayCategories(event) {
   event.preventDefault()
-  alert("Display Categories")
+  let string = '<h2>Categories</h2><ul id="categories">'
+  for (i = 0; i < categories.length; i++) {
+    string += "<li><h3><a href='/category_" + categories[i].id + ".html'>" + categories[i].name + "</a></h3></li>"
+  }
+  string += "</ul>"
+  main = document.getElementById("main")
+  main.innerHTML = string
 }
 
 function displayManufacturers(event) {
+  //alert("Hello")
+  event.preventDefault()
+  let thing = '<h2>Manufacturers</h2><ul id="categories">'
+  //htmlString ='<h2>Manufacturers</h2><ul id="categories"><li><h3><a href="manufacturer_5.html">BMW</a></h3></li><li><h3><a href="manufacturer_2.html">Honda</a></h3></li><li><h3><a href="manufacturer_1.html">Indian</a></h3></li><li><h3><a href="manufacturer_3.html">Triumph</a></h3></li><li><h3><a href="manufacturer_4.html">Victory</a></h3></li></ul>'
+
+  for (j = 0; j < manufacturers.length; j++) {
+    thing += "<li><h3><a href='/manufacturer_" + manufacturers[j].id + ".html'>" + manufacturers[j].name + "</a></h3></li>"
+  }
+
+  thing += "</ul>"
+
+  main = document.getElementById("main")
+  main.innerHTML = thing
 
 }
 
+document.addEventListener("DOMContentLoaded", bindNavBarClickHandlers)
 document.addEventListener("DOMContentLoaded", bindNavBarClickHandlers)
